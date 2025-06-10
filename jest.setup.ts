bc9b51b261559a5d9f03fd60ce16c6aec756c28a
Len Bekker <len.bekker@sybrin.com>
2025-06-10 16:01:38 +0200

    tests: use auth-lib v2.0.0

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,31 +1,29 @@
-const authLib = jest.requireActual('@frmscoe/auth-lib');
-
-class MockAuthenticationService {
-  getToken(u: string, p: string): Promise<string> {
-    if (u === 'blank') {
+import type { TazamaAuthentication } from '@tazama-lf/auth-lib';
+const authLib = jest.requireActual('@tazama-lf/auth-lib');
+class MockAuthenticationService extends (authLib.TazamaAuthentication as typeof TazamaAuthentication) {
+  getToken(...args: unknown[]): Promise<string> {
+    if (!args || args[0] === 'blank') {
       return Promise.resolve('');
-    } else if (u === 'reject') {
+    } else if (args[0] === 'reject') {
       return Promise.reject('REJECT');
     } else {
-      return Promise.resolve(`${u}${p}`);
+      return Promise.resolve(`${args[0]}`);
     }
   }
 }
 
-const mockedAuthLib = { ...authLib, AuthenticationService: MockAuthenticationService };
+const mockedAuthLib = { ...authLib, TazamaAuthentication: MockAuthenticationService };
 
-jest.mock('@frmscoe/auth-lib', () => mockedAuthLib);
-
-jest.mock('@tazama-lf/frms-coe-lib/lib/config', () => ({
-  validateEnvVar: jest.fn().mockReturnValue(''),
-}));
+jest.mock('@tazama-lf/auth-lib', () => {
+  return mockedAuthLib;
+});
 
 jest.mock('@tazama-lf/frms-coe-lib/lib/config', () => ({
   validateEnvVar: jest.fn().mockReturnValue(''),
   validateProcessorConfig: jest.fn().mockReturnValue({
     maxCPU: 1,
     functionName: 'auth-test',
-    nodeEnv: 'test'
+    nodeEnv: 'test',
   }),
   validateLogConfig: jest.fn().mockReturnValue({
     logstashLevel: '',
